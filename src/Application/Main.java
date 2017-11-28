dc150e8a92df682ec332bb94e78f88694e0d4099
Simon Wiering <simon.wiering@hva.nl>
2017-11-28 12:38:27 +0100

    geexporteerd naar netbeans

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -6,7 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private static Stage thePrimaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,8 +18,17 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        thePrimaryStage = primaryStage;
     }
 
+    public static void GoToScreen(String name) throws IOException {
+        //Dit is een test..
+        Parent root = FXMLLoader.load(Main.class.getResource(name));
+        thePrimaryStage.setTitle("Hello World");
+        thePrimaryStage.setScene(new Scene(root, 999, 999));
+        thePrimaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/Application/Menu.java
+++ src/Application/Menu.java
@@ -11,40 +11,26 @@ import java.io.IOException;
 
 public class Menu {
 
-    @FXML
-    Stage PrimaryStage;
-    @FXML
-    Scene scene;
 
     @FXML
     public void GoToLaf(ActionEvent event) throws IOException {
-        GoToScreen("LostAndFound.fxml");
+        Main.GoToScreen("LostAndFound.fxml");
     }
 
     @FXML
     public void GoToStats(ActionEvent event) throws IOException {
-        GoToScreen("Statistics.fxml");
+        Main.GoToScreen("Statistics.fxml");
     }
 
     @FXML
     public void GoToUsers(ActionEvent event) throws IOException {
-        GoToScreen("Users.fxml");
+        Main.GoToScreen("Users.fxml");
     }
 
     @FXML
     public void GoToImport(ActionEvent event) throws IOException {
-        GoToScreen("ImportExcel.fxml");
+        Main.GoToScreen("ImportExcel.fxml");
     }
 
-    public void GoToScreen(String name) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(name));
-        PrimaryStage.setTitle("Hello World");
-        PrimaryStage.setScene(new Scene(root, 999, 999));
-        PrimaryStage.show();
 
-//        Parent root = FXMLLoader.load(getClass().getResource(name));
-//        Scene ding = new Scene(root, 400, 400);
-//        stage.setScene(ding);
-//        stage.show();
-    }
 }
