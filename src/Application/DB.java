78ada4f92852feee4e7df8e9239182a2aaf40a19
Jair Zijp <jairzijp123@gmail.com>
2017-12-01 11:55:55 +0100

    trying to setup db connection

---
+++ src/Application/DB.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Application;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+/**
+ *
+ * @author jairz
+ */
+public class DB {
+    
+    private static final String DB_DEFAULT_DATABASE = "zzijpj1";
+    private static final String DB_DEFAULT_SERVER_URL = "oege.ie.hva.nl";
+    private static final String DB_DEFAULT_ACCOUNT = "zijpj1";
+    private static final String DB_DEFAULT_PASSWORD = "KKR#porqAalRBb";
+    
+    private final static String DB_DRIVER_URL = "com.mysql.jdbc.Driver";
+    private final static String DB_DRIVER_PREFIX = "jdbc:mysql://";
+    private final static String DB_DRIVER_PARAMETERS = "?useSSL=true";
+    
+    
+     private Connection connection = null;
+    
+    // set for verbose logging of all queries
+    private boolean verbose = true;
+    
+    // remembers the first error message on the connection 
+    private String errorMessage = null; 
+    
+    // constructors
+    public DB() {
+        this(DB_DEFAULT_DATABASE, DB_DEFAULT_SERVER_URL, DB_DEFAULT_ACCOUNT, DB_DEFAULT_PASSWORD);
+    }
+    
+    public DB(String dbName) {
+        this(dbName, DB_DEFAULT_SERVER_URL, DB_DEFAULT_ACCOUNT, DB_DEFAULT_PASSWORD);
+    }
+    
+    public DB(String dbName, String account, String password) {
+        this(dbName, DB_DEFAULT_SERVER_URL, account, password);
+    }
+    
+     public DB(String dbName, String serverURL, String account, String password) {
+        try {
+            // verify that a proper JDBC driver has been installed and linked
+            if (!selectDriver(DB_DRIVER_URL)) {
+                return;
+            }
+
+            if (password == null) {
+                password = "";
+            }
+
+            // establish a connection to a named database on a specified server	
+            String connStr = DB_DRIVER_PREFIX + serverURL + "/" + dbName + DB_DRIVER_PARAMETERS;
+            log("Connecting " + connStr);
+            this.connection = DriverManager.getConnection(connStr, account, password);
+            
+        } catch (SQLException eSQL) {
+            error(eSQL);
+            this.close();
+        }
+    }
+    
+    public final void close() {
+        
+        if (this.connection == null) {
+            // db has been closed earlier already
+            return;
+        }
+        try {
+            this.connection.close();
+            this.connection = null;
+            this.log("Data base has been closed");
+        } catch (SQLException eSQL) {
+            error(eSQL);
+        }
+    }
+    
+     /***
+     * elects proper loading of the named driver for database connections.
+     * This is relevant if there are multiple drivers installed that match the JDBC type
+     * @param driverName    the name of the driver to be activated.    
+     * @return              indicates whether a suitable driver is available
+     */
+    private Boolean selectDriver(String driverName) 
+    {
+        try {
+            Class.forName(driverName);
+            // Put all non-prefered drivers to the end, such that driver selection hits the first
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            while (drivers.hasMoreElements()) {
+                Driver d = drivers.nextElement();
+                if (!d.getClass().getName().equals(driverName)) {   // move the driver to the end of the list
+                    DriverManager.deregisterDriver(d);
+                    DriverManager.registerDriver(d);
+                }
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            error(ex);
+            return false;
+        }
+        return true;
+    }
+
+    
+     /***
+     * echoes a message on the system console, if run in verbose mode
+     * @param message
+     */
+    public void log(String message) {
+        if (isVerbose()) {
+            System.out.println("MyJDBC: " + message);
+        }
+    }
+
+    /***
+     * echoes an exception and its stack trace on the system console.
+     * remembers the message of the first error that occurs for later reference.
+     * closes the connection such that no further operations are possible.
+     * @param e
+     */
+    public final void error(Exception e) {
+        String msg = "MyJDBC-" + e.getClass().getName() + ": " + e.getMessage();
+        
+        // capture the message of the first error of the connection
+        if (this.errorMessage == null) {
+            this.errorMessage = msg;
+        }
+        System.out.println(msg);
+        e.printStackTrace();
+
+        // if an error occurred, close the connection to prevent further operations
+        this.close();
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    
+}
