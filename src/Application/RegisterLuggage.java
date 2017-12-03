76e1e1f6dffe64f364d9614807a5ce89326304e5
Jair Zijp <jairzijp123@gmail.com>
2017-12-03 21:17:02 +0100

    register working but needs improvements (validation, code cleanup and feedback)

--- src/Application/RegisterLuggage.java
+++ src/Application/RegisterLuggage.java
@@ -12,7 +12,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -21,6 +24,8 @@ import javafx.scene.control.TextField;
  */
 public class RegisterLuggage {
         
+    @FXML
+    private AnchorPane Step1;
     @FXML
     private TextField NameField;
     @FXML
@@ -36,10 +41,36 @@ public class RegisterLuggage {
     @FXML
     private TextField PhoneNumberField;
     
+    /* Step 2. */
+    @FXML
+    private AnchorPane Step2;
+    @FXML
+    private TextField LabelNumberField;
+    @FXML
+    private TextField TypeField;
+    @FXML
+    private TextField FlightIDField;
+    @FXML
+    private TextField BrandField;
+    @FXML
+    private TextField LocationField;
+    @FXML
+    private DatePicker FoundDateField;
+    @FXML
+    private DatePicker LostDateField;
+    @FXML
+    private TextArea StatusField;
+    @FXML
+    private TextField CompesationField;
+    @FXML
+    private TextField SpecialFeaturesField;
+    @FXML 
+    private TextField LostLocationField;
     
     @FXML
     private boolean saveLuggage(ActionEvent event) throws IOException, SQLException {
         
+        /* Step 1. */
         final String name = NameField.getText().toString();
         final String email = EmailField.getText().toString();
         final String city = CityField.getText().toString();
@@ -48,19 +79,39 @@ public class RegisterLuggage {
         final String phoneNumber = PhoneNumberField.getText().toString();
         final String zipcode = ZipcodeField.getText().toString();
         
+        /* Step 2. */
+        final String labelNumber = LabelNumberField.getText().toString();
+        final String type = TypeField.getText().toString();
+        final String flightId = FlightIDField.getText().toString();
+        final String brand = BrandField.getText().toString();
+        final String location = LocationField.getText().toString();
+        final String foundDate = FoundDateField.getValue().toString();
+        final String lostDate = LostDateField.getValue().toString();
+        final String status = StatusField.getText().toString();
+        final String compesation = CompesationField.getText().toString();
+        final String specialFeatures = SpecialFeaturesField.getText().toString();
+        final String lostLocation = LostLocationField.getText().toString();
+        
         DB database = new DB();
         
-        String query = String.format("INSERT INTO customer (name, email, address, city, zipcode, country, phone)" +
+        String queryStep1 = String.format("INSERT INTO customer (name, email, address, city, zipcode, country, phone)" +
                 "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')",
                 name, email, address, city, zipcode, country, phoneNumber);
         
-        database.executeUpdateQuery(query);
+        String queryStep2 = String.format("INSERT INTO luggage (label_number, type, brand, special_features, owner, status, location, lost_location, found_date, lost_date, compesation)" +
+                "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
+                labelNumber, type, brand, specialFeatures, name, status, location, lostLocation, foundDate, lostDate, compesation );
+        
+        database.executeUpdateQuery(queryStep1);
+        database.executeUpdateQuery(queryStep2);
         
         return true;
     }
     
     @FXML
-    private void nextStep() {
+    private void nextStep(ActionEvent event) throws IOException {
+        Step1.setVisible(false);
+        Step2.setVisible(true);
         
     }
     
