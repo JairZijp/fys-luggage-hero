952aed9174de956e38f321d4dcd278c092cc629a
Simon <simon.wiering@hva.nl>
2017-12-03 19:48:31 +0100

    Updated users functionality

--- src/Application/Users.java
+++ src/Application/Users.java
@@ -6,14 +6,31 @@
 package Application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
 /**
  *
  * @author Simon
  */
-public class Users {
+public class Users implements Initializable{
+    
+    public void initialize(URL url, ResourceBundle rb){
+       DB Connection = new DB();
+       //Simpele query om te kijken of er een record dubbel is, niet specifiek welke record.
+       String sql = String.format("SELECT `ID`, `username`, `name`, `role`, `email` FROM user");
+       ResultSet queryResult = Connection.executeResultSetQuery(sql);
+       
+       Connection.close();
+    }
+
     /**
      * Redirects to the userscreate.fxml
      * @param event
--- src/Application/UsersCreate.java
+++ src/Application/UsersCreate.java
@@ -1,11 +1,16 @@
 package Application;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -42,15 +47,18 @@ public class UsersCreate implements Initializable {
         ObservableList obList = FXCollections.observableList(list);
         RoleField.getItems().clear();
         RoleField.setItems(obList);
-    }
-    
+    }    
     
     /**
      * Function to create/edit a user. After saving redirects back to Users.FXML
      * @throws IOException 
      */
     @FXML
-    private boolean SaveUser(ActionEvent event) throws IOException, SQLException{
+    private boolean SaveUser(ActionEvent event) throws IOException, SQLException, NoSuchAlgorithmException{
+        //use this variable to check if user can be registered
+        boolean passed = true;
+        String validationErrors = "";
+        
         //To String after every getText so it can be checked on being empty
         final String username = UserNameField.getText().toString();
         final String name = NameField.getText().toString();
@@ -59,43 +67,68 @@ public class UsersCreate implements Initializable {
         final String password = PasswordField.getText().toString();
         final String confirmPassword = ConfirmPasswordField.getText().toString();
         
-        
-        if(name == null || username == null || email == null){
+        //values can not be null
+        if(name == null || username == null || email == null || role == null || password == null){
             ValidationLabel.setText("Fill in all fields");
             return false;
         }
-//        if(email != )
+        
+        //mail has to match regex
+        Pattern ptr = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                Pattern.CASE_INSENSITIVE);
+        if(! ptr.matcher(email).matches()){
+            ValidationLabel.setText("E-mail is invalid");
+            return false;
+        }
+        
+        //password and confirmpassword has to be same values
         if(password == null ? confirmPassword != null : !password.equals(confirmPassword)){
             ValidationLabel.setText("Passwords do not match.");
             return false;
         }
         
-        //Make Connection
-        DB dbconn = new DB();
+        if(!UserExists(username, email)){
+            InsertUser(username, name, role, password, email);
+        }else{
+            ValidationLabel.setText("User already exists with this username/email");
+            return false;
+        }
                 
-        String sql = String.format("INSERT INTO user (username, name, role, password, email)" +
-                "VALUES ('%s', '%s', '%s', '%s', '%s')",
-                username, name, role, email, password);
-        
-        //execute query and close connection     
-        dbconn.executeUpdateQuery(sql);
-        dbconn.close();
-        
         //Navigate back to the list
         Main.GoToScreen("Users.fxml");
         return true;
     }
     
+    public static String HashPassword(String Password) throws NoSuchAlgorithmException{
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(Password.getBytes(), 0, Password.length());    
+        return new BigInteger(1, m.digest()).toString(16);
+    }
     
-    /**
-     * Looks if a user is logged in and redirects to Inloggen.fxml
-     * @throws java.io.IOException
-     */
-    public void Authenticate() throws IOException{
+    private boolean UserExists(String Username, String Email) throws SQLException {
+        boolean exists;
+        DB Connection = new DB();
+        //Simpele query om te kijken of er een record dubbel is, niet specifiek welke record.
+        String sql = String.format("SELECT * FROM user WHERE `username` = '%s' OR `email` = '%s'", Username, Email);
+        ResultSet queryResult = Connection.executeResultSetQuery(sql);
         
+        exists = queryResult.first();
         
-        //Redirect back to inloggen.fxml to get a user logged in
-        Main.GoToScreen("Inloggen.fxml");
+        Connection.close();
+        
+        return exists;
+    }
+    
+    private void InsertUser(String Username, String Name, String Role, String Password, String Email) throws NoSuchAlgorithmException{
+        //Make Connection                
+        DB Connection = new DB(); 
+        String sql = String.format("INSERT INTO user (username, name, role, password, email)" +
+                "VALUES ('%s', '%s', '%s', '%s', '%s')",
+                Username, Name, Role, HashPassword(Password), Email);
+        
+        //execute query and close connection     
+        Connection.executeUpdateQuery(sql);
+        Connection.close();
     }
 }
 
